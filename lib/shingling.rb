de2b4d62aa10ca01f836c6a48050ae27d888c7c5
vad <vad4msiu@gmail.com>
2011-11-05 15:47:52 +0400

    rename shingle to shingling

---
+++ lib/shingling.rb
@@ -0,0 +1,50 @@
+require "shingling/version"
+
+module Shingling
+  class Shingling
+    def initialize(content = '', options = {})
+      @content = content
+      @shingle_length = options[:shingle_length] || 5
+      @stop_words = options[:stop_words] || []
+      @downcase = options[:downcase] || false
+      @replace_chars = options[:replace_chars] || {}
+    end
+
+    def each_shinglings
+      word = ""
+      char_flag = false
+      shingling = []
+      position_end_words = []
+      position_start, position_end = 0, 0
+
+      @content.each_char do |char|
+        char = @replace_chars[char] if @replace_chars.key? char
+        if char !~ /[[:word:]]+/
+          char_flag = true
+        else
+          if char_flag
+            if !stop_word?(word) || word !~ /\S/
+              shingling << (@downcase ? Unicode::downcase(word) : word)
+              word = ""
+              position_end_words << position_end
+              if shingling.size == @shingle_length
+                yield(shingling, position_start, position_end - 1)
+                position_start = position_end_words.shift
+                shingling.shift
+              end
+            end
+            char_flag = false
+          end
+          word << char
+        end
+        position_end += 1
+      end        
+    end
+
+    private
+
+    def stop_word? word
+      @stop_words.include? word
+    end  
+  end
+end
---
+++ spec/shingling_spec.rb
@@ -0,0 +1,5 @@
+require "shingling"
+
+describe Shingling do
+  it "Create test"
+end
